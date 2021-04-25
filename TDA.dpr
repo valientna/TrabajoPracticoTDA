793cf4dbac10ef15414f9c38a11d1b1ff16a3fee
Franco Fernandez <macufer5@hotmail.com>
2021-04-25 11:59:32 -0300

    .

--- TDA.dpr
+++ TDA.dpr
@@ -3,10 +3,11 @@ program TDA;
 uses
   Vcl.Forms,
   ArreglosDinamicos in 'Objetos\ArreglosDinamicos.pas',
-    Principal in 'Vistas\Principal.pas' {PrincipalForm},
+  Principal in 'Vistas\Principal.pas' {PrincipalForm},
   VisualUno in 'Vistas\VisualUno.pas' {Form1},
   VisualDos in 'Vistas\VisualDos.pas' {Form2},
-  VisualTres in 'Vistas\VisualTres.pas' {Form3};
+  VisualTres in 'Vistas\VisualTres.pas' {Form3},
+  Matrices in 'Objetos\Matrices.pas';
 
 {$R *.res}
 
